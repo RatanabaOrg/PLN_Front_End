9b55dd7a6634507ac124d149ae7efc989694866f
JuliaGayotto <juliagayotto@gmail.com>
2024-09-11 00:57:08 -0300

    deixando a tabela de historico de acessos dinamica

--- frontend/src/pages/accessHistory/index.js
+++ frontend/src/pages/accessHistory/index.js
@@ -1,45 +1,58 @@
-import Header from '../../components/header'
-import './index.css'
+import Header from "../../components/header";
+import "./index.css";
 
 function AccessHistory() {
-  let list;
-    return (
-      <>
-        <Header/>
-        <div id="access-history-container">
-          <h1 id="access-history-title">Histórico de acessos </h1>
-          <table>
-                    <thead>
-                        <tr>
-                          <th scope="col">Data</th>
-                          <th scope="col">Nome</th>
-                          <th scope="col">Área</th>
-                        </tr>
-                    </thead>
-                        {/* {typeof list !== 'undefined' && list.map((value) => {
-                            return !value.envio ? */}
-                                    <tbody>
-                                        <tr>
-                                          <td data-label="Data">15/09/2024 23:00:00</td>
+  let list = [
+    {
+      id: 1,
+      nome: "Lara Caires",
+      datatime: "15/09/2024 23:00:00",
+      area: "Área 3",
+    },
+    {
+      id: 2,
+      nome: "Fábio Rodrigues",
+      datatime: "15/09/2024 23:00:00",
+      area: "Área 2",
+    },
+    {
+      id: 3,
+      nome: "Marcos Paulo Lobo",
+      datatime: "15/09/2024 23:00:00",
+      area: "Área 1",
+    },
+  ];
+  return (
+    <>
+      <Header />
+      <div id="access-history-container">
+        <h1 id="access-history-title">Histórico de acessos </h1>
+        <table>
+          <thead>
+            <tr>
+              <th scope="col">Data</th>
+              <th scope="col">Nome</th>
+              <th scope="col">Área</th>
+            </tr>
+          </thead>
+          <tbody>
+            {typeof list !== "undefined" &&
+              list.map((value) => {
+                return !value.envio ? (
+                  <tr key={value.id}>
+                    <td data-label="Data">{value.datatime}</td>
 
-                                          <td data-label="Nome">Lara Caires</td>
+                    <td data-label="Nome">{value.nome}</td>
 
-                                          <td data-label="Área">Área 3</td>
-                                        </tr>
-                                        <tr>
-                                          <td data-label="Data">15/09/2024 23:00:00</td>
+                    <td data-label="Área">{value.area}</td>
+                  </tr>
+                ) : null;
+              })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
 
-                                          <td data-label="Nome">Fábio Rodrigues</td>
-
-                                          <td data-label="Área">Área 2</td>
-                                        </tr>
-                                    </tbody>
-                            {/* :null */}
-                        {/* })} */}
-                    </table>
-        </div>
-      </>
-    );
-  }
-  
 export default AccessHistory;
