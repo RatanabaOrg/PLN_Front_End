6bf8888ed32b7e7f8f465604db2c4d63092979f1
Desduh <eduardofalandess@gmail.com>
2024-09-19 20:43:55 -0300

    finalizacao da conexao entre interface de visualizacao de acessos e back

--- frontend/src/pages/accessHistory/index.js
+++ frontend/src/pages/accessHistory/index.js
@@ -1,33 +1,31 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Header from "../../components/header";
 import "./index.css";
 
 function AccessHistory() {
-  let list = [
-    {
-      id: 1,
-      nome: "Lara Caires",
-      datatime: "15/09/2024 23:00:00",
-      area: "Área 3",
-    },
-    {
-      id: 2,
-      nome: "Fábio Rodrigues",
-      datatime: "15/09/2024 23:00:00",
-      area: "Área 2",
-    },
-    {
-      id: 3,
-      nome: "Marcos Paulo Lobo",
-      datatime: "15/09/2024 23:00:00",
-      area: "Área 1",
-    },
-  ];
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/visualizar');
+        console.log(response);
+        
+        setList(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar o histórico de acessos:", error);
+      }
+    };
+
+    fetchData();
+  }, []); 
 
   return (
     <>
       <Header />
       <div id="access-history-container">
-        <h1 id="access-history-title">Histórico de acessos </h1>
+        <h1 id="access-history-title">Histórico de acessos</h1>
         <table>
           <thead>
             <tr>
@@ -37,18 +35,21 @@ function AccessHistory() {
             </tr>
           </thead>
           <tbody>
-            {typeof list !== "undefined" &&
-              list.map((value) => {
-                return !value.envio ? (
+            {list.length > 0 ? (
+              list.map((value) => (
+                !value.envio && (
                   <tr key={value.id}>
-                    <td data-label="Data">{value.datatime}</td>
-
+                    <td data-label="Data">{value.data}</td>
                     <td data-label="Nome">{value.nome}</td>
-
                     <td data-label="Área">{value.area}</td>
                   </tr>
-                ) : null;
-              })}
+                )
+              ))
+            ) : (
+              <tr>
+                <td colSpan="3">Nenhum histórico encontrado.</td>
+              </tr>
+            )}
           </tbody>
         </table>
       </div>
@@ -57,3 +58,26 @@ function AccessHistory() {
 }
 
 export default AccessHistory;
+
+
+
+//   [
+//   {
+//     id: 1,
+//     nome: "Lara Caires",
+//     datatime: "15/09/2024 23:00:00",
+//     area: "Área 3",
+//   },
+//   {
+//     id: 2,
+//     nome: "Fábio Rodrigues",
+//     datatime: "15/09/2024 23:00:00",
+//     area: "Área 2",
+//   },
+//   {
+//     id: 3,
+//     nome: "Marcos Paulo Lobo",
+//     datatime: "15/09/2024 23:00:00",
+//     area: "Área 1",
+//   },
+// ];
